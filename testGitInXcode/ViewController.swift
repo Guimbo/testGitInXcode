f70b858ff76c30d85bf345acfdbdf46e97479231
Kenai <guiaszul@gmail.com>
2018-05-29 16:27:51 -0300

    firas local commit

--- testGitInXcode/ViewController.swift
+++ testGitInXcode/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var msg = "Hello, Git"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
